95c61bf2d251418ec6ead9989b60f42787ab2cc1
Mahtab <aghdamifard.mahtab@gmail.com>
2023-01-17 21:36:02 +0330

    Eleventh Commit: Trying to Connect SearchFragment To HomeScreen

--- app/src/main/java/com/shariaty/melobit/fragments/SearchFragment.java
+++ app/src/main/java/com/shariaty/melobit/fragments/SearchFragment.java
@@ -7,15 +7,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.shariaty.melobit.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SearchFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class SearchFragment extends Fragment {
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -28,6 +26,7 @@ public class SearchFragment extends Fragment {
 
     public SearchFragment() {
         // Required empty public constructor
+
     }
 
     /**
@@ -54,6 +53,7 @@ public class SearchFragment extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
     }
 
