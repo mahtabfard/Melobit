720cdf40fe1233b809abd72916ff199ec8bfe367
Mahtab <aghdamifard.mahtab@gmail.com>
2023-01-17 19:59:08 +0330

    Fifth Commit:Connecting HomeScreen To Search Fragment

--- app/src/main/java/com/shariaty/melobit/MainActivity.java
+++ app/src/main/java/com/shariaty/melobit/MainActivity.java
@@ -1,14 +1,36 @@
 package com.shariaty.melobit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.shariaty.melobit.fragments.SearchFragment;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button tophits;
+    ImageView searchbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        searchbtn=findViewById(R.id.btnsearch);
+        tophits=findViewById(R.id.topbtn);
+
+        searchbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment frag = new SearchFragment();
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.home_layout, frag).commit();
+            }
+        });
+
     }
 }
