dbd0ff3d71b210230448750f740a86e6a1c6db9f
Mahtab <aghdamifard.mahtab@gmail.com>
2023-01-18 22:21:07 +0330

    Twenty Fifth Commit: Connecting Adapter To RecyclerView(New Songs)

--- app/src/main/java/com/shariaty/melobit/fragments/NewFragment.java
+++ app/src/main/java/com/shariaty/melobit/fragments/NewFragment.java
@@ -1,66 +1,66 @@
 package com.shariaty.melobit.fragments;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.shariaty.melobit.Models.MelobitData;
 import com.shariaty.melobit.R;
+import com.shariaty.melobit.Request;
+import com.shariaty.melobit.adapter.Adapter;
+import com.shariaty.melobit.manager.RequestManager;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link NewFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.List;
+
+
 public class NewFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    RecyclerView recyclerView3;
+    ProgressDialog dialog;
+    RequestManager manager;
+    Context context;
 
     public NewFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment NewFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static NewFragment newInstance(String param1, String param2) {
-        NewFragment fragment = new NewFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new, container, false);
+        View view = inflater.inflate(R.layout.fragment_top_day, container, false);
+        recyclerView3 = view.findViewById(R.id.topdayrecycler);
+        context = view.getContext();
+        dialog = new ProgressDialog(context);
+        dialog.setTitle("Loading...⌛");
+        manager = new RequestManager(context);
+        manager.getFixture(request);
+
+        return view;
     }
+    final Request request = new Request() {
+        @Override
+        public void didFetch(List<MelobitData> list, String status) {
+            dialog.dismiss();
+            recyclerView3.setHasFixedSize(true);
+            recyclerView3.setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.VERTICAL , false));
+            RecyclerView.Adapter adapter = new Adapter(context,list);
+            recyclerView3.setAdapter(adapter);
+        }
+
+        @Override
+        public void didError(String status) {
+            dialog.dismiss();
+            Toast.makeText(context, status, Toast.LENGTH_SHORT).show();
+        }
+    };
 }
