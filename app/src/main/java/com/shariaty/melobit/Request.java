0fe234ebb53589ed6ea383cd1c9f9de3634dff30
Mahtab <aghdamifard.mahtab@gmail.com>
2023-01-18 17:52:40 +0330

    Sixteenth Commit:Interfaces

---
+++ app/src/main/java/com/shariaty/melobit/Request.java
@@ -0,0 +1,10 @@
+package com.shariaty.melobit;
+
+import com.shariaty.melobit.Models.MelobitData;
+
+import java.util.List;
+
+public interface Request {
+    void didFetch(List<MelobitData> list, String status);
+    void didError(String status);
+}
