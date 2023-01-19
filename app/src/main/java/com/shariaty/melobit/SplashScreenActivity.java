8c5df7fcd6251d69945d42f99f986e75ddadea8c
Mahtab <aghdamifard.mahtab@gmail.com>
2023-01-19 21:51:33 +0330

    Twenty Seventh Commit:Colors And Fonts

--- app/src/main/java/com/shariaty/melobit/SplashScreenActivity.java
+++ app/src/main/java/com/shariaty/melobit/SplashScreenActivity.java
@@ -16,25 +16,11 @@ import org.w3c.dom.Text;
 public class SplashScreenActivity extends AppCompatActivity {
 
     private static int SPLASH_SCREEN=3000;
-
-     Animation topAnim,bottomAnim;
-     ImageView logoImg;
-     TextView txtsplash;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
-
-        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
-        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
-
-        logoImg=findViewById(R.id.logoimg);
-        txtsplash=findViewById(R.id.txtsplashscreen);
-
-        logoImg.setAnimation(topAnim);
-        txtsplash.setAnimation(bottomAnim);
-
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
